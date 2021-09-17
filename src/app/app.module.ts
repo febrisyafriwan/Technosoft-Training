4d6df6f8df6f7b5d9cf72c0837f6976e22037136
febrisyafriwan <febrisyafriwan@gmail.com>
2021-09-17 10:27:06 +0700

    1 file modified, 8 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ContactComponent } from './contact.component';
-import { ProductComponent } from './product.component';
-import { ErrorComponent } from './error.component';
-import { ProductDetailComponent } from './product-detail.component';
-import { ProductService } from './product.service';
 import { HomeModule } from './home';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HomeModule, AppRoutingModule],
-  providers: [ProductService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
